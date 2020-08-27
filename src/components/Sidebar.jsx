4cadfdac88c385cbce75865a7a84d8c531eb76d2
abarran03 <58741178+abarran03@users.noreply.github.com>
2020-08-26 23:16:41 -0600

    new version

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Sidebar =({children})=>(
+    <div className="Sidebar">
+      <div className="Sidebar-container">
+        <div className="Sidebar-item">
+            {children}
+
+
+
+        </div>  
+
+
+
+        </div>  
+
+
+
+    </div>
+
+);
+
+export default Sidebar;
