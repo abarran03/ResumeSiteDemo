1591d509338fe8ec6e4ab671133f2814b70a1b21
=Allan <=abarran03@gmail.com>
2020-10-01 00:01:31 -0600

    update package

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { hasUncaughtExceptionCaptureCallback } = require('process');
-
 module.exports = {
     entry: './src/index.js',
     output:{
